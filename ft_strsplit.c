978a3112fdf821abd071a32a298e3481cbb6ed36
Lamia BOUKROUH <lboukrou@e1r13p1.42.fr>
2020-02-12 19:00:54 +0100

    update some functions

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,13 +6,42 @@
 /*   By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 19:19:15 by lboukrou          #+#    #+#             */
-/*   Updated: 2017/12/17 15:08:02 by lboukrou         ###   ########.fr       */
+/*   Updated: 2019/12/21 19:03:03 by lboukrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-char	**ft_strsplit(char const *s, char c)
+static char		**ft_free_all(char ***tab)
+{
+	int		i;
+
+	i = 0;
+	if (!*tab)
+		return (NULL);
+	while ((*tab)[i])
+	{
+		free((*tab)[i]);
+		i++;
+	}
+	free(*tab);
+	return (NULL);
+}
+
+static size_t	ft_words_count(char const *s, char c)
+{
+	size_t	words;
+	size_t	i;
+
+	i = -1;
+	words = 0;
+	while (++i < ft_strlen(s))
+		words = (s[i] != c && (!i || s[i - 1] == c)) ? words + 1 : words;
+	return (words);
+}
+
+char			**ft_strsplit(char const *s, char c)
 {
 	char	**tab;
 	size_t	words;
@@ -21,20 +50,21 @@ char	**ft_strsplit(char const *s, char c)
 
 	if (s == NULL || !c)
 		return (NULL);
-	i = -1;
-	words = 0;
-	while (++i < ft_strlen(s))
-		words = (s[i] != c && (s[i - 1] == c || !i)) ? words + 1 : words;
+	words = ft_words_count(s, c);
 	if ((tab = malloc(sizeof(char*) * (words + 1))) == NULL)
-		return (NULL);
+		return (ft_free_all(&tab));
 	i = -1;
 	words = -1;
 	while (s[++i])
 	{
-		if (s[i] != c && (s[i - 1] == c || !i))
+		if (s[i] != c && (!i || s[i - 1] == c))
 			y = i;
 		if (s[i] != c && (s[i + 1] == c || !s[i + 1]))
+		{
 			tab[++words] = ft_strsub(s, y, i - y + 1);
+			if (!tab[words])
+				return (ft_free_all(&tab));
+		}
 	}
 	tab[words + 1] = NULL;
 	return (tab);
